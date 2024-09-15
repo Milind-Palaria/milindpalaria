372a2037a16dd7ee478083c3ce80fc308423f136
milind-palaria <lit2021047@iiitl.ac.in>
2024-09-15 10:33:10 +0530

    Preojet Setup

---
+++ app/page.js
@@ -0,0 +1,17 @@
+'use client'
+
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+
+  return (
+    <main className={styles.main}>
+      <Image 
+        src="/bg.jpg"
+        fill={true}
+        alt="background"
+      />
+    </main>
+  )
+}
